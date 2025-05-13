8ac8cd15fd81437dea398823ff57a2e3bfc97608
Babul Mirdha <babumirdha@gmail.com>
2025-05-13 23:37:39 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Counter from './components/counter'
 
 function App() {
+
   const [count, setCount] = useState(0)
+  const [isCounterShow, setIsCounterShow] = useState(false)
+
+  const handleShowCounter = () => {
+    setIsCounterShow(true)
+  }
 
   return (
     <>
@@ -18,15 +25,15 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+
+        {<Counter />}
+
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+
+        <button onclick={handleShowCounter()} > Show Counter</button>
+
       </p>
     </>
   )
